13b67d71cbd670446621ac392af5fbb9fd087936
Eduardo Rodrigues <44787022+Eduaaado@users.noreply.github.com>
2020-06-14 22:08:20 -0300

    NewTransactionActivity view holding

--- app/app/src/main/java/com/example/expensestracker/views/NewTransactionActivity.java
+++ app/app/src/main/java/com/example/expensestracker/views/NewTransactionActivity.java
@@ -1,6 +1,10 @@
 package com.example.expensestracker.views;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,10 +13,34 @@ import com.example.expensestracker.R;
 
 public class NewTransactionActivity extends AppCompatActivity {
 
+    private ViewHolder mViewHolder = new ViewHolder();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_transaction);
 
+        this.mViewHolder.editTransTitle = findViewById(R.id.edit_transaction_title);
+        this.mViewHolder.editTransAmount = findViewById(R.id.edit_transaction_amount);
+        this.mViewHolder.spinTransType = findViewById(R.id.spin_transaction_type);
+        this.mViewHolder.btnTransConfirm = findViewById(R.id.btn_transaction_confirm);
+
+        this.mViewHolder.btnTransConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // Create Entry
+
+                finish();
+            }
+        });
+
+    }
+
+    private static class ViewHolder {
+        EditText editTransTitle;
+        EditText editTransAmount;
+        Spinner spinTransType;
+        Button  btnTransConfirm;
     }
 }
